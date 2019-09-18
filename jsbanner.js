be13ef0d1ae9bcefed23cf1d275e2fd58c6020f4
William Blacknall <williamblacknall@gmail.com>
2019-09-18 11:01:07 -0400

    Removed 'for' loop, as it wasn't being used at all.

--- jsbanner.js
+++ jsbanner.js
@@ -1,9 +1,7 @@
 //Returns a square banner wrapped around given text in function. Ridiculously brute forced.
 const printBanner = (text) => {
     const box = "*"
-    for (i = 0; i < text.length + 1; i++){
-        return `${box.repeat(text.length + 2)}\n${box + text + box}\n${box.repeat(text.length + 2)}` 
+    return `${box.repeat(text.length + 2)}\n${box + text + box}\n${box.repeat(text.length + 2)}` 
     }
-}
 
 console.log(printBanner("As fast as fast can get"))
