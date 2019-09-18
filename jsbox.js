8b7d98e0f332e92b64a88290907a214fb718b333
William Blacknall <williamblacknall@gmail.com>
2019-09-18 10:39:18 -0400

    Creates a box

---
+++ jsbox.js
@@ -0,0 +1,15 @@
+//prints a box with function-defined parameters
+const printBox = (width, height) =>{
+    const box = "*"
+    const empty = " "
+    for (i = 0; i < height + 1; i++){
+        if (i === 0 || i === height ){
+            console.log(box.repeat(width))
+        } 
+        else {
+            console.log(box + (empty.repeat(width -2)) + box)
+        }
+    }
+}
+
+printBox(5,10)
